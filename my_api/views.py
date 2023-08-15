b4935faef238d9b42326906eedebd9622944364c
Berdishukur <boboqulovberdishukur@gmail.com>
2023-08-15 09:13:44 +0500

    create drf

--- my_api/views.py
+++ my_api/views.py
@@ -1,9 +1,26 @@
 from rest_framework import viewsets
 from django.contrib.auth.models import User
-
-from .serializers import UserSerializer
+from .models import Book,Fanlar,Yunalishlar,Unversitetlar
+from .serializers import *
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
+    queryset = User.objects.all().order_by('date_joined')
     serializer_class = UserSerializer
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all().order_by("id")
+    serializer_class = BookSerilizer
+class FanViewSet(viewsets.ModelViewSet):
+    queryset = Fanlar.objects.all().order_by("id")
+    serializer_class = FanlarSerilizer
+
+class YunalishViewSet(viewsets.ModelViewSet):
+    queryset = Yunalishlar.objects.all().order_by("id")
+    serializer_class = YunalishSerilizer
+class UnversityViewSet(viewsets.ModelViewSet):
+    queryset = Unversitetlar.objects.all().order_by("id")
+    serializer_class = UnversitySerilizer
+
+
+
+
