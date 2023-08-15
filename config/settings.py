6d9d59c5b4ae1929c0c64012aaf457268e004a7e
Berdishukur <boboqulovberdishukur@gmail.com>
2023-08-15 09:12:28 +0500

    create drf

--- config/settings.py
+++ config/settings.py
@@ -74,13 +74,22 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'firstDRF_db',
+        'USER':'firstDRF_user',
+        'PASSWORD':'berdi1234',
+        'HOST':'127.0.0.1',
+        'PORT':'5432',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
